75860cca401f7913766d63c0eeba0b15333eb338
t0ntin <115113636+t0ntin@users.noreply.github.com>
2025-06-09 11:27:52 -0500

    Menus toggle correctly.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const question1El = document.querySelector('.question1');
+const answer1El = document.querySelector('.answer1');
+const question2El = document.querySelector('.question2');
+const answer2El = document.querySelector('.answer2');
+const question3El = document.querySelector('.question3');
+const answer3El = document.querySelector('.answer3');
+const question4El = document.querySelector('.question4');
+const answer4El = document.querySelector('.answer4');
+
+ question1El.addEventListener('click', () => {
+  answer1El.classList.toggle('visible');
+ });
+
+ question2El.addEventListener('click', () => {
+  answer2El.classList.toggle('visible');
+ });
+ question3El.addEventListener('click', () => {
+  answer3El.classList.toggle('visible');
+ });
+ question4El.addEventListener('click', () => {
+  answer4El.classList.toggle('visible');
+ });
